6c1e5e9292514738b73b6ab30be2558bd3403f93
Shazril Shohaimi <shazril@live.jp>
2023-10-13 12:04:01 +0800

    update for github page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://strategic-access.github.io',
   integrations: [starlight({
     title: 'OpServ Docs',
 	customCss: [
