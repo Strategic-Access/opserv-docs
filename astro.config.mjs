a50438b17807a6ef4ca762887730fe644ce5772e
Shazril Shohaimi <shazril@live.jp>
2023-12-12 13:07:47 +0800

    skip image processing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://docs.mstime.com.my',
+  image: {
+    service: passthroughImageService()
+  },
   integrations: [starlight({
     title: 'OpServ Docs',
 	customCss: [
